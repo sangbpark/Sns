97ca11a52db0343c937588a49adeb1bb047298a5
sangbpark <qkrtkdqo94@gmail.com>
2024-11-01 21:45:30 +0900

    memo 회원가입 로그인 20241101

--- src/main/java/com/sns/user/UserRestController.java
+++ src/main/java/com/sns/user/UserRestController.java
@@ -5,10 +5,13 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sns.common.EncryptUntils;
+import com.sns.user.Enitity.UserEntity;
 import com.sns.user.bo.UserBO;
 
 @RequestMapping("/user")
@@ -19,13 +22,33 @@ public class UserRestController {
 	
 	@GetMapping("/is-duplicate-id")
 	public Map<String, Object> isDuplicateId(
-			@RequestParam String loginId) {
+			@RequestParam("loginId") String loginId) {
 		
 		boolean isDuplicateId = userBO.isDupilcateId(loginId);
 		
 		Map<String, Object> result = new HashMap<>();
 		result.put("code", 200);
 		result.put("is_duplicate_id", isDuplicateId);
+		return result;
+	}
+	
+	@PostMapping("/sign-up")
+	public Map<String, Object> signUp(
+			@RequestParam("loginId") String loginId,
+			@RequestParam("password") String password,
+			@RequestParam("name") String name,
+			@RequestParam("email") String email) {
+		
+		String hashedPassword = EncryptUntils.md5(password);
+		UserEntity user = userBO.addUser(loginId, hashedPassword, name, email);
+		Map<String, Object> result = new HashMap<>();
+		if (user != null) {
+			result.put("code", 200);
+			result.put("result", "성공");
+		} else {
+			result.put("code", 500);
+			result.put("error_message", "회원가입에 실패했습니다.");
+		}
 		return result;
 	}
 }
--- src/main/java/com/sns/user/bo/UserBO.java
+++ src/main/java/com/sns/user/bo/UserBO.java
@@ -3,6 +3,7 @@ package com.sns.user.bo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.sns.user.Enitity.UserEntity;
 import com.sns.user.repository.UserRepository;
 
 @Service
@@ -12,5 +13,20 @@ public class UserBO {
 	
 	public boolean isDupilcateId(String loginId) {
 		return userRepository.findByLoginId(loginId).isPresent();
+	}
+	
+	public UserEntity addUser(String loginId, String password, String name, String email) {
+	    
+		try {
+			return userRepository.save(
+	        		UserEntity.builder()
+	        		.loginId(loginId)
+	        		.password(password)
+	        		.name(name)
+	        		.email(email)
+	        		.build());
+	    } catch (org.springframework.dao.DataIntegrityViolationException e) {
+	        return null;
+	    }
 	}
 }
